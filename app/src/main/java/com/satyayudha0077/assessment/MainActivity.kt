b3964bcdb9bff6861b92d092a04751543a30bd64
Satya Yudha Prabaswara <satya.yprabaswara@users.noreply.github.com>
2026-04-05 08:10:40 +0700

    Menambahkan data buah + Membuat Top App Bar

--- app/src/main/java/com/satyayudha0077/assessment/MainActivity.kt
+++ app/src/main/java/com/satyayudha0077/assessment/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.satyayudha0077.assessment
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.satyayudha0077.assessment.model.Buah
 import com.satyayudha0077.assessment.ui.theme.AssessmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AssessmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen() {
+    val data = listOf(
+        Buah("Apel", R.drawable.apel, R.string.apel),
+        Buah("Alpukat", R.drawable.alpukat, R.string.alpukat),
+        Buah("Jeruk", R.drawable.jeruk, R.string.jeruk),
+        Buah("Jambu", R.drawable.jambu, R.string.jambu),
+        Buah("Pisang", R.drawable.pisang, R.string.pisang)
+    )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "HEII",
         modifier = modifier
     )
 }
 
+
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     AssessmentTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
