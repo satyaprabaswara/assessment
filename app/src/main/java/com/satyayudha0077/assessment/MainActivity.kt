c08a92d45ceaa63de7005177df6a455bf3bd4aaf
Satya Yudha Prabaswara <satya.yprabaswara@users.noreply.github.com>
2026-04-06 22:59:18 +0700

    Memindahkan Main Screen

--- app/src/main/java/com/satyayudha0077/assessment/MainActivity.kt
+++ app/src/main/java/com/satyayudha0077/assessment/MainActivity.kt
@@ -1,37 +1,11 @@
 package com.satyayudha0077.assessment
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.satyayudha0077.assessment.model.Buah
-import com.satyayudha0077.assessment.ui.screen.MainScreen
+import androidx.navigation.compose.rememberNavController
+import com.satyayudha0077.assessment.navigation.SetupNavGraph
 import com.satyayudha0077.assessment.ui.theme.AssessmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +14,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AssessmentTheme {
-                MainScreen()
+                val navController = rememberNavController()
+                SetupNavGraph(navController)
             }
         }
     }
